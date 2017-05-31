2d7ffb4db6e8cef8f81d6e3297ec9449b5117be3
antonio11gp <antonio11gp@gmail.com>
2017-05-31 00:11:50 -0600

    graficas

--- app/src/main/java/com/example/kenneth/smartbudget/GeneralFragment.java
+++ app/src/main/java/com/example/kenneth/smartbudget/GeneralFragment.java
@@ -8,9 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -21,87 +26,48 @@ import java.util.Random;
 
 
 public class GeneralFragment extends Fragment {
-    private BarChart barChart;
-    private ArrayList<String> dates;
-    private Random random;
-    private ArrayList<BarEntry> barEntries;
+
+    private PieChart pieChart;
+    private ArrayList<Entry> entries;
+    ArrayList<String> labels;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_general, container, false);
-        String dato1="2017/01/05";
-        String dato2="2017/05/01";
 
-        barChart = (BarChart) view.findViewById(R.id.bargraph);
-        createRandomBarGraph(dato1, dato2);
+        pieChart = (PieChart) view.findViewById(R.id.chart);
+        entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(12f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
 
+        PieDataSet dataset = new PieDataSet(entries, "# of Calls");
+        labels = new ArrayList<String>();
+
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        PieData data = new PieData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        pieChart.setDescription("Description");
+        pieChart.setData(data);
+
+        pieChart.animateY(5000);
+
+        pieChart.saveToGallery("/sd/mychart.jpg", 85);
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-    }
-    public void createRandomBarGraph(String Date1, String Date2){
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
-
-        try {
-            Date date1 = simpleDateFormat.parse(Date1);
-            Date date2 = simpleDateFormat.parse(Date2);
-
-            Calendar mDate1 = Calendar.getInstance();
-            Calendar mDate2 = Calendar.getInstance();
-            mDate1.clear();
-            mDate2.clear();
-
-            mDate1.setTime(date1);
-            mDate2.setTime(date2);
-
-            dates = new ArrayList<>();
-            dates = getList(mDate1,mDate2);
-
-            barEntries = new ArrayList<>();
-            float max = 0f;
-            float value = 0f;
-            random = new Random();
-            for(int j = 0; j< dates.size();j++){
-                max = 100f;
-                value = random.nextFloat()*max;
-                barEntries.add(new BarEntry(value,j));
-            }
-
-        }catch(ParseException e){
-            e.printStackTrace();
-        }
-
-        BarDataSet barDataSet = new BarDataSet(barEntries,"Dates");
-        BarData barData = new BarData(dates,barDataSet);
-        barChart.setData(barData);
-        barChart.setDescription("Último mes");
-
-    }
-
-    public ArrayList<String> getList(Calendar startDate, Calendar endDate){
-        ArrayList<String> list = new ArrayList<String>();
-        while(startDate.compareTo(endDate)<=0){
-            list.add(getDate(startDate));
-            startDate.add(Calendar.DAY_OF_MONTH,1);
-        }
-        return list;
-    }
-
-    public String getDate(Calendar cld){
-        String curDate = cld.get(Calendar.YEAR) + "/" + (cld.get(Calendar.MONTH) + 1) + "/"
-                +cld.get(Calendar.DAY_OF_MONTH);
-        try{
-            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(curDate);
-            curDate =  new SimpleDateFormat("yyy/MM/dd").format(date);
-        }catch(ParseException e){
-            e.printStackTrace();
-        }
-
-        return curDate;
-
     }
 }
